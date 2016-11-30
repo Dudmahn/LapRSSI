6558742710bf5ba47c90995b515e0dc8e3322c70
Hyper-Quad <mike@fancyfoam.com>
2016-11-29 19:57:03 -0600

    Update LapRSSI_v1.ino

--- src/LapRSSI_v1.ino
+++ src/LapRSSI_v1.ino
@@ -1,11 +1,11 @@
 IntervalTimer myTimer;
 int heartbeat;
-float timervalue = 1;
-float timersave[8];
-int rssi[8];
-int pilotrssithreshold[8] = {350,350,350,350,350,350,350,350,};
+float timer_value = 1;
+float timer_save[8];
+int pilot_rssi[8];
+int pilot_rssi_threshold[8] = {350,350,350,350,350,350,350,350,};
 int pin[8] = {9,8,7,6,5,4,3,2};
-int pilotnumber;
+int pilot_number;
 int scan = 0;
 
 void setup()
@@ -17,11 +17,11 @@ void setup()
 
 void timer(void)
 {
-  timervalue = timervalue + 0.001;
+  timer_value = timer_value + 0.001;
   for (int i = 0; i < 8; i++){
   {
     rssi[i] = analogRead(pin[i]);
-    if (rssi[i] > pilotrssithreshold[i]) {timersave[i] = timervalue;}
+    if (rssi[i] > pilot_rssi_threshold[i]) {timer_save[i] = timer_value;}
   }
 }
 
@@ -34,7 +34,7 @@ void loop()
       Serial.print("\t");
       Serial.print(heartbeat);
       Serial.print("\t");
-      Serial.print(timervalue);
+      Serial.print(timer_value);
       Serial.print("\t");
       Serial.println("LapRSSI");
       delay(1000);
@@ -42,21 +42,21 @@ void loop()
      }
     else
     { 
-      if (timersave[i] > 0)
+      if (timer_save[i] > 0)
       {
         noInterrupts();
         Serial.print(" @");
         Serial.print("\t");  
         Serial.print(heartbeat);
         Serial.print("\t");
-        Serial.print(pilotnumber[i]);
+        Serial.print(pilot_number[i]);
         Serial.print("\t");
-        Serial.print(timersave[i], 3); // needs time stamp
+        Serial.print(timer_save[i], 3); // needs time stamp
         Serial.print("\t");
-        Serial.print(rssi[i]);
+        Serial.print(pilot_rssi[i]);
         Serial.print("\t");
         Serial.println("LapRSSI");
-        timersave[i] = 0;
+        timer_save[i] = 0;
         interrupts();
         delay(1000);
         heartbeat = heartbeat + 1;
@@ -67,8 +67,8 @@ void loop()
     if (Serial.peek() == '@') {
       Serial.read();
       pilot = Serial.parseInt();
-      pilotfrequency = Serial.parseInt();
-      pilotrssithreshold = Serial.parseInt();
+      pilot_frequency = Serial.parseInt();
+      pilot_rssi_threshold = Serial.parseInt();
     }
     If (Serial.peek() == '&') {
       Serial.read();
