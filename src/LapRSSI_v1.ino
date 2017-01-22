de27e28aa088dee183707cc44b65457a3bb9be97
Hyper-Quad <mike@fancyfoam.com>
2017-01-22 08:36:51 -0600

    Update LapRSSI_v1.ino

--- src/LapRSSI_v1.ino
+++ src/LapRSSI_v1.ino
@@ -1,85 +1,143 @@
-IntervalTimer myTimer;
-int heartbeat;
-float timer_value = 1;
+#include "channels.h"
+#include "pinAssignments.h"
+#include "rx5808spi.h"
+int heartbeat = 1;
+float timer_value = 0;
 float timer_save[8];
 int pilot_rssi[8];
-int pilot_rssi_threshold[8] = {350,350,350,350,350,350,350,350,};
-int pin[8] = {9,8,7,6,5,4,3,2};
-int pilot_number;
+int rssi_threshold[8] = {550,550,550,550,550,550,550,550};
+int pin[8] = {0,1,2,3,4,5,6,7};
+int pilot;
+int pilot_frequency[8];
+int pilot_number[8];
 int scan = 0;
+int cal;
+int rssi_save[8];
+
+elapsedMillis beat;
+elapsedMillis beat1;
+IntervalTimer myTimer;
 
 void setup()
 {
+  pinMode(spiDataPin, OUTPUT);
+  pinMode(slaveSelectPin, OUTPUT);
+  pinMode(spiClockPin, OUTPUT);
   Serial.begin(9600); //opens serial port, sets data rate to 9600 bps
   myTimer.begin(timer, 1000);  // increment timer every 0.001 seconds
-  heartbeat = 1;
 }
 
 void timer(void)
 {
   timer_value = timer_value + 0.001;
-  for (int i = 0; i < 8; i++){
+  for (int i = 0; i < 8; i++)
   {
-    pilot_rssi[i] = analogRead(pin[i]);
-    if (pilot_rssi[i] > pilot_rssi_threshold[i]) {timer_save[i] = timer_value;}
+    pilot_rssi[i] = analogRead(i);
+    if (pilot_rssi[i] > rssi_threshold[i]) 
+    {
+      rssi_save[i] = pilot_rssi[i];
+      timer_save[i] = timer_value;
+    }
   }
 }
 
 void loop()
 {
-  for (int i = 0; i < 7; i++){
-    if (i == 6) 
+  if (beat >= 1000)
+  {
+    Serial.print(" #");
+    Serial.print("\t"); 
+    Serial.print(heartbeat);
+    Serial.print("\t");
+    Serial.print(timer_value);
+    Serial.print("\t");
+    Serial.println("LapRSSI");
+    heartbeat = heartbeat + 1;
+    beat = 0;
+  }
+  if (beat1 >= 500)
+  {
+  for  (int i = 0; i < 7; i++)
+  {
+    if (timer_save[i] != 0)
     {
-      Serial.print(" #");
+      Serial.print(" @");
       Serial.print("\t");
       Serial.print(heartbeat);
       Serial.print("\t");
-      Serial.print(timer_value);
+      Serial.print(pilot_number[i]);
+      Serial.print("\t");
+      Serial.print(timer_save[i], 2);
+      Serial.print("\t");
+      Serial.print(rssi_save[i]);
+      Serial.print("\t");
+      Serial.print("Freq=");
+      Serial.print(pilot_frequency[i]);
+      Serial.print("\t");
+      Serial.print("rssiT");
+      Serial.print(rssi_threshold[i]);
       Serial.print("\t");
       Serial.println("LapRSSI");
-      delay(1000);
-      heartbeat = heartbeat + 1;
-     }
-    else
-    { 
-      if (timer_save[i] > 0)
-      {
-        noInterrupts();
-        Serial.print(" @");
-        Serial.print("\t");  
-        Serial.print(heartbeat);
-        Serial.print("\t");
-        Serial.print(pilot_number[i]);
-        Serial.print("\t");
-        Serial.print(timer_save[i], 3); // needs time stamp
-        Serial.print("\t");
-        Serial.print(pilot_rssi[i]);
-        Serial.print("\t");
-        Serial.println("LapRSSI");
-        timer_save[i] = 0;
-        interrupts();
-        delay(1000);
-        heartbeat = heartbeat + 1;
-      }
+      interrupts();
+      timer_save[i] = 0;
     }
   }
-  if (Serial.available() > 0) {
-    if (Serial.peek() == '@') {
+  beat1 = 0;
+  }
+  if (Serial.available() > 0) 
+  {
+    if (Serial.peek() == '@') // @ pilot pilot_frequency rssi_threshold
+    {
       Serial.read();
       pilot = Serial.parseInt();
-      pilot_frequency = Serial.parseInt();
-      pilot_rssi_threshold = Serial.parseInt();
+
+      pilot_frequency[pilot] = Serial.parseInt();
+      rssi_threshold[pilot] = Serial.parseInt();
+
+      for  (int i = 0; i < 48; i++)
+        {
+          if (pilot_frequency[pilot] == channelFreqTable[i])
+            {
+              setChannelModule(i);
+            }
+        }
     }
-    If (Serial.peek() == '&') {
+    if (Serial.peek() == '&') // frequency scanner.
+    {
       Serial.read();
       Serial.read();
       scan = Serial.parseInt();
-        if (scan == 1) {
-          // insert scan code here. 
-        }
+      if (scan == 1)
+      {
+        // insert scan code here.
+      }
     }
-    while (Serial.available() > 0) {
+    if (Serial.peek() == '$') // pilot RSSI calibration
+    {
       Serial.read();
+      pilot = Serial.parseInt();
+      Serial.read();
+      cal = 0;
+      for  (int i = 0; i < 5; i++)
+      {
+        pilot_rssi[pilot] = analogRead(pin[pilot]);
+        cal = (cal + pilot_rssi[pilot]);
+        delay (1000);
+        Serial.print(cal);
+      }
+      cal = (cal / 5);
+      Serial.print(" $");
+      Serial.print("\t"); 
+      Serial.print(pilot);
+      Serial.print("\t");
+      Serial.print(cal);
+      Serial.print("\t");
+      Serial.println("LapRSSI");
+      rssi_threshold[pilot] = cal;
     }
+  }
+  while (Serial.available() > 0) 
+  {
+    Serial.read();
   }
 }
