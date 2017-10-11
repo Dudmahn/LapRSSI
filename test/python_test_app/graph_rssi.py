3ba00685152bac858d361b4583d6c1753383547f
Dudmahn <lillyste@yahoo.com>
2017-10-11 11:07:22 -0400

    Python test script now handles all 8 receiver nodes
    Add support for 8 receiver nodes
    Avoid configuration of frequencies, node enable/disable, and most
    settings when starting up. Only configure RSSI report interval. Other
    settings must be configured via docklight.

--- test/python_test_app/graph_rssi.py
+++ test/python_test_app/graph_rssi.py
@@ -25,8 +25,9 @@ import pyttsx3
 import winsound
 
 # Configuration
-comPort = 'COM65'
-#comPort = 'COM50'
+#comPort = 'COM65'
+comPort = 'COM50'
+#comPort = 'COM75'
 historySeconds = 150.0
 sayTimes = False
 doBeep = True
@@ -34,8 +35,8 @@ doBeep = True
 # Initialize TTS engine
 tts = pyttsx3.init()
 
-tts.say('graph r s s i starting up')
-tts.runAndWait()
+#tts.say('graph r s s i starting up')
+#tts.runAndWait()
 
 # Initialize graphing engine
 win = pg.GraphicsWindow(title='LapRSSI Plotter')
@@ -55,30 +56,45 @@ freq2 = 0
 freq3 = 0
 freq4 = 0
 freq5 = 0
+freq6 = 0
+freq7 = 0
+freq8 = 0
 
 rssi1 = []
 rssi2 = []
 rssi3 = []
 rssi4 = []
 rssi5 = []
+rssi6 = []
+rssi7 = []
+rssi8 = []
 
 lap1 = []
 lap2 = []
 lap3 = []
 lap4 = []
 lap5 = []
+lap6 = []
+lap7 = []
+lap8 = []
 
 threshHi1 = 0
 threshHi2 = 0
 threshHi3 = 0
 threshHi4 = 0
 threshHi5 = 0
+threshHi6 = 0
+threshHi7 = 0
+threshHi8 = 0
 
 thresh1 = []
 thresh2 = []
 thresh3 = []
 thresh4 = []
 thresh5 = []
+thresh6 = []
+thresh7 = []
+thresh8 = []
 
 def addPoint(a, t, p):
     a.append({'x': t, 'y': p})
@@ -90,8 +106,8 @@ def trimOldPoints(a):
 
 def processSerialMsg(msg):
     global curTime
-    global threshHi1, threshHi2, threshHi3, threshHi4, threshHi5
-    global freq1, freq2, freq3, freq4, freq5
+    global threshHi1, threshHi2, threshHi3, threshHi4, threshHi5, threshHi6, threshHi7, threshHi8
+    global freq1, freq2, freq3, freq4, freq5, freq6, freq7, freq8
     #print(msg)
     msg = msg.rstrip('\r\n')
     fields = msg.split('\t')
@@ -107,6 +123,9 @@ def processSerialMsg(msg):
             freq3 = int(fields[3])
             freq4 = int(fields[4])
             freq5 = int(fields[5])
+            freq6 = int(fields[6])
+            freq7 = int(fields[7])
+            freq8 = int(fields[8])
 
     elif fields[0] == '%RSS':
         if len(fields) == 11:
@@ -131,6 +150,18 @@ def processSerialMsg(msg):
                 addPoint(rssi5, curTime, int(fields[7]))
                 if threshHi5 > 0:
                     addPoint(thresh5, curTime, threshHi5)
+            if fields[8]:
+                addPoint(rssi6, curTime, int(fields[8]))
+                if threshHi6 > 0:
+                    addPoint(thresh6, curTime, threshHi6)
+            if fields[9]:
+                addPoint(rssi7, curTime, int(fields[9]))
+                if threshHi7 > 0:
+                    addPoint(thresh7, curTime, threshHi7)
+            if fields[10]:
+                addPoint(rssi8, curTime, int(fields[10]))
+                if threshHi8 > 0:
+                    addPoint(thresh8, curTime, threshHi8)
             updatePlot()
 
 
@@ -158,6 +189,15 @@ def processSerialMsg(msg):
             if(idx == 4):
                 threshHi5 = threshHi
                 addPoint(lap5, curTime, peakRssi)
+            if(idx == 5):
+                threshHi6 = threshHi
+                addPoint(lap6, curTime, peakRssi)
+            if(idx == 6):
+                threshHi7 = threshHi
+                addPoint(lap7, curTime, peakRssi)
+            if(idx == 7):
+                threshHi8 = threshHi
+                addPoint(lap8, curTime, peakRssi)
             updatePlot()
             
             if doBeep:
@@ -177,18 +217,27 @@ def updatePlot():
     trimOldPoints(rssi3)
     trimOldPoints(rssi4)
     trimOldPoints(rssi5)
+    trimOldPoints(rssi6)
+    trimOldPoints(rssi7)
+    trimOldPoints(rssi8)
 
     trimOldPoints(lap1)
     trimOldPoints(lap2)
     trimOldPoints(lap3)
     trimOldPoints(lap4)
     trimOldPoints(lap5)
+    trimOldPoints(lap6)
+    trimOldPoints(lap7)
+    trimOldPoints(lap8)
 
     trimOldPoints(thresh1)
     trimOldPoints(thresh2)
     trimOldPoints(thresh3)
     trimOldPoints(thresh4)
     trimOldPoints(thresh5)
+    trimOldPoints(thresh6)
+    trimOldPoints(thresh7)
+    trimOldPoints(thresh8)
 
     # Clear and re-add the legend each time around
     legend.scene().removeItem(legend)
@@ -199,18 +248,27 @@ def updatePlot():
     p1.plot(rssi3, pen=(0,0,255),   name='Pilot 3: ' + str(freq3), clear=False)
     p1.plot(rssi4, pen=(180,180,0), name='Pilot 4: ' + str(freq4), clear=False)
     p1.plot(rssi5, pen=(180,0,180), name='Pilot 5: ' + str(freq5), clear=False)
+    p1.plot(rssi6, pen=(0,180,180), name='Pilot 6: ' + str(freq6), clear=False)
+    p1.plot(rssi7, pen=(255,180,0), name='Pilot 7: ' + str(freq7), clear=False)
+    p1.plot(rssi8, pen=(255,255,255), name='Pilot 8: ' + str(freq8), clear=False)
 
     p1.plot(lap1, pen=None, symbolPen=(255,0,0), symbolBrush=(255,0,0), symbol='x', clear=False)
     p1.plot(lap2, pen=None, symbolPen=(0,255,0), symbolBrush=(0,255,0), symbol='x', clear=False)
     p1.plot(lap3, pen=None, symbolPen=(0,0,255), symbolBrush=(0,0,255), symbol='x', clear=False)
     p1.plot(lap4, pen=None, symbolPen=(180,180,0), symbolBrush=(180,180,0), symbol='x', clear=False)
     p1.plot(lap5, pen=None, symbolPen=(180,0,180), symbolBrush=(180,0,180), symbol='x', clear=False)
+    p1.plot(lap6, pen=None, symbolPen=(0,180,180), symbolBrush=(0,180,180), symbol='x', clear=False)
+    p1.plot(lap7, pen=None, symbolPen=(255,180,0), symbolBrush=(255,180,0), symbol='x', clear=False)
+    p1.plot(lap8, pen=None, symbolPen=(255,255,255), symbolBrush=(255,255,255), symbol='x', clear=False)
 
     p1.plot(thresh1, pen=(150,0,0), clear=False)
     p1.plot(thresh2, pen=(0,150,0), clear=False)
     p1.plot(thresh3, pen=(0,0,150), clear=False)
     p1.plot(thresh4, pen=(100,100,0), clear=False)
     p1.plot(thresh5, pen=(100,0,100), clear=False)
+    p1.plot(thresh6, pen=(0,100,100), clear=False)
+    p1.plot(thresh7, pen=(150,100,0), clear=False)
+    p1.plot(thresh8, pen=(150,150,150), clear=False)
 
 
 class serialThread(pg.QtCore.QThread):
@@ -229,22 +287,6 @@ class serialThread(pg.QtCore.QThread):
 
         ser.reset_input_buffer()
 
-        # Set frequencies
-        #ser.write('#FRA\t5806\t5925\t5843\t5752\t5695\t5917\t5917\t5917\r\n'.encode('utf-8'))   # Custom
-        ser.write('#FRA\t5800\t5800\t5800\t5800\t5800\t5800\t5800\t5800\r\n'.encode('utf-8'))   # All Fatshark 4
-        #ser.write('#FRA\t5658\t5695\t5760\t5800\t5880\t5917\t5917\t5917\r\n'.encode('utf-8'))   # IMD6C
-        #ser.write('#FRA\t5658\t5695\t5732\t5769\t5806\t5843\t5880\t5917\r\n'.encode('utf-8'))   # Raceband 8
-        #ser.write('#FRA\t5658\t5658\t5658\t5658\t5658\t5658\t5658\t5658\r\n'.encode('utf-8'))   # All Raceband 1
-        time.sleep(0.250)
-        
-        # Configure the timing device. Parameters are:
-        #    rssi_report_interval
-        #    cal_offset
-        #    cal_thresh
-        #    trig_thresh
-        ser.write('#CFG\t250\t140\t300\t100\r\n'.encode('utf-8'))
-        time.sleep(0.250)
-        
         # Query version
         ser.write('?VER\r\n'.encode('utf-8'))
         time.sleep(0.250)
@@ -252,13 +294,17 @@ class serialThread(pg.QtCore.QThread):
         # Query configuration
         ser.write('?CFG\r\n'.encode('utf-8'))
         time.sleep(0.250)
-
+        
+        # Query enabled receiver modules
+        ser.write('?REN\r\n'.encode('utf-8'))
+        time.sleep(0.250)
+        
         # Query frequency assignment
         ser.write('?FRA\r\n'.encode('utf-8'))
         time.sleep(0.250)
 
-        # Query initial RSSIs
-        ser.write('?RSS\r\n'.encode('utf-8'))
+        # Enable RSSI reports every 500ms (leave other parameters unchanged)
+        ser.write('#CFG\t500\t\t\t\t\r\n'.encode('utf-8'))
         time.sleep(0.250)
         
         # Read data from serial port
